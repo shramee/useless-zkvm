785494bbf8d8a4e97d211a5c8062efb4d367a8a5
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-02-01 15:08:46 +0530

    chore: vm tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use Op::{Add, Div, Mul, Push, Sub};
+use Useless_ZKVM::vm::run_vm;
+
 fn main() {
-    println!("Hello, world!");
+    let program = Vec::from([
+        Push(3),
+        Push(7),
+        Add,
+        Push(2),
+        Mul,
+        Push(3434),
+        Div,
+        Push(567),
+        Sub,
+    ]);
+
+    run_vm(program.into());
 }
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,6 +1,6 @@
 enum Op {
     // Push value to Stack
-    Push,
+    Push(u32),
     // Arithmetic ops
     Add,
     Sub,
@@ -8,7 +8,22 @@ enum Op {
     Div,
 }
 
-struct VM {
-    instructions: Vec<Op>,
-    stack: Vec<u32>,
+pub struct VM {
+    program: Vec<Op>,
+}
+
+impl Into<VM> for Vec<Op> {
+    fn into(self) -> VM {
+        VM { program: self }
+    }
+}
+
+pub fn run_vm(program: VM) {
+    program.program.iter().for_each(|op| match op {
+        Op::Push(val) => println!("Pushing {}", val),
+        Op::Add => println!("Adding"),
+        Op::Sub => println!("Subtracting"),
+        Op::Mul => println!("Multiplying"),
+        Op::Div => println!("Dividing"),
+    });
 }
