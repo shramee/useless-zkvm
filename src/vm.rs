a13d57429de886f8af3a7eb21564610ee1b749cc
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-02-01 16:41:06 +0530

    chore: expose private properties

--- src/vm.rs
+++ src/vm.rs
@@ -9,23 +9,36 @@ pub enum Op {
 }
 
 pub struct VM {
-    program: Vec<Op>,
-    pub log_n_rows: u32,
-    // stack: Vec<Op>,
+    _program: Vec<Op>,
+    _log_n_rows: u32,
+}
+
+impl VM {
+    pub fn new(program: Vec<Op>) -> Self {
+        program.into()
+    }
+
+    pub fn program(&self) -> &Vec<Op> {
+        &self._program
+    }
+
+    pub fn log_n_rows(&self) -> u32 {
+        self._log_n_rows
+    }
 }
 
 impl Into<VM> for Vec<Op> {
     fn into(self) -> VM {
         let rows = self.len();
         VM {
-            program: self,
-            log_n_rows: rows.next_power_of_two().trailing_zeros(),
+            _program: self,
+            _log_n_rows: rows.next_power_of_two().trailing_zeros(),
         }
     }
 }
 
 pub fn run_vm(vm: VM) {
-    vm.program.iter().for_each(|op| match op {
+    vm._program.iter().for_each(|op| match op {
         Op::Push(val) => println!("Pushing {}", val),
         Op::Add => println!("Adding"),
         Op::Sub => println!("Subtracting"),
