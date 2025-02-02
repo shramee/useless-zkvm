fb9cb9e51313872a477bb3968ba96a4a556c7b72
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-02-02 15:46:40 +0530

    feat: vm runner

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,4 @@
-use stwo_prover::core::fields::m31::BaseField;
+use stwo_prover::core::fields::m31::{BaseField, M31};
 
 pub enum Op {
     // Push value to Stack
@@ -27,6 +27,44 @@ impl VM {
     pub fn log_n_rows(&self) -> u32 {
         self._log_n_rows
     }
+
+    pub fn run(&self) -> M31 {
+        let mut stack = Vec::new();
+        self._program.iter().for_each(|op| match op {
+            Op::Push(val) => stack.push(*val),
+            Op::Add => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let res = a + b;
+                stack.push(b);
+                stack.push(res);
+            }
+            Op::Sub => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let res = a - b;
+                stack.push(b);
+                stack.push(res);
+            }
+            Op::Mul => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let res = a * b;
+                stack.push(b);
+                stack.push(res);
+            }
+            Op::Div => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                let res = a / b;
+                stack.push(b);
+                stack.push(res);
+            }
+        });
+        println!("Result: {:?}", stack.pop().unwrap());
+
+        return stack.pop().unwrap();
+    }
 }
 
 impl Into<VM> for Vec<Op> {
@@ -37,14 +75,4 @@ impl Into<VM> for Vec<Op> {
             _log_n_rows: rows.next_power_of_two().trailing_zeros(),
         }
     }
-}
-
-pub fn run_vm(vm: VM) {
-    vm._program.iter().for_each(|op| match op {
-        Op::Push(val) => println!("Pushing {}", val),
-        Op::Add => println!("Adding"),
-        Op::Sub => println!("Subtracting"),
-        Op::Mul => println!("Multiplying"),
-        Op::Div => println!("Dividing"),
-    });
 }
