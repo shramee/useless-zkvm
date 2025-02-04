110e063b5bda71fdc4f7202ffbf0b054cf3c8bad
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-02-04 17:50:35 +0530

    test: fix prover test

--- src/prover.rs
+++ src/prover.rs
@@ -138,57 +138,54 @@ mod tests {
     }
 
     #[test_log::test]
-    fn test_wide_fib_prove_with_blake() {
-        for log_n_instances in 2..=6 {
-            let config = PcsConfig::default();
-            // Precompute twiddles.
-            let twiddles = SimdBackend::precompute_twiddles(
-                CanonicCoset::new(log_n_instances + 1 + config.fri_config.log_blowup_factor)
-                    .circle_domain()
-                    .half_coset,
-            );
+    fn test_vm_prove_with_blake() {
+        let vm = dummy_program();
+        let config = PcsConfig::default();
+        // Precompute twiddles.
+        let twiddles = SimdBackend::precompute_twiddles(
+            CanonicCoset::new(vm.log_n_rows() + 1 + config.fri_config.log_blowup_factor)
+                .circle_domain()
+                .half_coset,
+        );
 
-            // Setup protocol.
-            let prover_channel = &mut Blake2sChannel::default();
-            let mut commitment_scheme =
-                CommitmentSchemeProver::<SimdBackend, Blake2sMerkleChannel>::new(config, &twiddles);
+        // Setup protocol.
+        let prover_channel = &mut Blake2sChannel::default();
+        let mut commitment_scheme =
+            CommitmentSchemeProver::<SimdBackend, Blake2sMerkleChannel>::new(config, &twiddles);
 
-            // Preprocessed trace
-            let mut tree_builder = commitment_scheme.tree_builder();
-            tree_builder.extend_evals([]);
-            tree_builder.commit(prover_channel);
+        // Preprocessed trace
+        let mut tree_builder = commitment_scheme.tree_builder();
+        tree_builder.extend_evals([]);
+        tree_builder.commit(prover_channel);
 
-            // Trace.
-            let vm = dummy_program();
-            let trace = generate_vm_trace(&vm);
-            let mut tree_builder = commitment_scheme.tree_builder();
-            tree_builder.extend_evals(trace);
-            tree_builder.commit(prover_channel);
+        // Trace.
+        let trace = generate_vm_trace(&vm);
+        let mut tree_builder = commitment_scheme.tree_builder();
+        tree_builder.extend_evals(trace);
+        tree_builder.commit(prover_channel);
 
-            // Prove constraints.
-            let component = VMComponent::new(
-                &mut TraceLocationAllocator::default(),
-                vm,
-                SecureField::zero(),
-            );
+        // Prove constraints.
+        let component = VMComponent::new(
+            &mut TraceLocationAllocator::default(),
+            vm,
+            SecureField::zero(),
+        );
 
-            let proof = prove::<SimdBackend, Blake2sMerkleChannel>(
-                &[&component],
-                prover_channel,
-                commitment_scheme,
-            )
-            .unwrap();
+        let proof = prove::<SimdBackend, Blake2sMerkleChannel>(
+            &[&component],
+            prover_channel,
+            commitment_scheme,
+        )
+        .unwrap();
 
-            // Verify.
-            let verifier_channel = &mut Blake2sChannel::default();
-            let commitment_scheme =
-                &mut CommitmentSchemeVerifier::<Blake2sMerkleChannel>::new(config);
+        // Verify.
+        let verifier_channel = &mut Blake2sChannel::default();
+        let commitment_scheme = &mut CommitmentSchemeVerifier::<Blake2sMerkleChannel>::new(config);
 
-            // Retrieve the expected column sizes in each commitment interaction, from the AIR.
-            let sizes = component.trace_log_degree_bounds();
-            commitment_scheme.commit(proof.commitments[0], &sizes[0], verifier_channel);
-            commitment_scheme.commit(proof.commitments[1], &sizes[1], verifier_channel);
-            verify(&[&component], verifier_channel, commitment_scheme, proof).unwrap();
-        }
+        // Retrieve the expected column sizes in each commitment interaction, from the AIR.
+        let sizes = component.trace_log_degree_bounds();
+        commitment_scheme.commit(proof.commitments[0], &sizes[0], verifier_channel);
+        commitment_scheme.commit(proof.commitments[1], &sizes[1], verifier_channel);
+        verify(&[&component], verifier_channel, commitment_scheme, proof).unwrap();
     }
 }
